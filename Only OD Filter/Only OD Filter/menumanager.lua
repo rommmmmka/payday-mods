4f84ef0ba6ccf550cb7dde9d8657c97e1e77b062
Raman Kravets <rommmmmka.m@gmail.com>
2023-07-10 14:03:49 +0300

    only od 1.6

--- Only OD Filter/Only OD Filter/menumanager.lua
+++
@@ -1,3 +0,0 @@
-Hooks:PostHook(MenuCallbackHandler, "chocie_one_down_filter", "dzedbaradzed_onlyodfilter_refresh", function()
-	managers.network.matchmake:search_lobby(managers.network.matchmake:search_friends_only())
-end)
